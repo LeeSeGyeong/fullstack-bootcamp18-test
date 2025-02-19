d62466b1a541a23244625bb6420a496fa25b8855
sekyeong <sekoonglee@gmail.com>
2025-02-19 20:29:43 +0900

    Feat: Add User Schema

---
+++ server/models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  id: String,
+  email: String,
+  nick: String,
+  token: String,
+  provider: { type: String, default: "google" }
+});
+
+module.exports = mongoose.model("User", userSchema);
