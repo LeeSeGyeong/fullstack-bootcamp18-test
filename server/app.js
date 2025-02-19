7d45e9e3d9090e0cddb6f545a8370a59e2421b5c
sekyeong <sekoonglee@gmail.com>
2025-02-19 20:34:51 +0900

    Feat: Add server setting

--- server/app.js
+++ server/app.js
@@ -1,22 +1,50 @@
 require("dotenv").config();
 const createError = require("http-errors");
 const express = require("express");
+const session = require("express-session");
 const path = require("path");
-
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-
-const indexRouter = require("./routes/index");
+const cors = require("cors");
 
 const app = express();
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const passport = require("passport");
+
+const indexRouter = require("./routes/index");
+const authRouter = require("./routes/auth");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
+
+app.io = require("socket.io")();
 app.use("/", express.static(path.join(__dirname, "../client/build")));
+const { connectDB } = require("./config/db");
+const { connectSocket } = require("./socket/socket");
+
+connectDB();
+connectSocket(app.io);
+
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: true,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use("/", indexRouter);
+app.use("/", authRouter);
 
 app.use(function(req, res, next) {
   next(createError(404));
@@ -26,8 +54,13 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  res.status(err.status || 500);
-  res.render("error");
+  if (req.headers["content-type"] === "application/json") {
+    res.json({
+      result: "error",
+    });
+  } else {
+    res.render("error");
+  }
 });
 
 module.exports = app;
